21396a5cc8d4f5bcd916d02589ba38824fc2835f
Dulguun Narantuya <dulguunbatbold1@gmail.com>
2025-10-02 22:56:04 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import {Home} from "./components/Home";
-import {AdminRSVP} from "./components/AdminRSVP";
+import { Home } from "./components/Home";
+import { AdminRSVP } from "./components/AdminRSVP";
 
 export default function App() {
     return (
-        <Router>
+        <Router basename="/wedding">
             <Routes>
-                <Route path="/wedding/" element={<Home />} />
-                <Route path="/wedding/admin" element={<AdminRSVP />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/admin" element={<AdminRSVP />} />
             </Routes>
         </Router>
     );
